da4ed286c095c0f9df82150e3412225f63bdb6b0
ritchiechen <ritchiechen@tencent.com>
2019-04-05 23:26:41 +0800

    create a pypi entry

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name = 'serverlessplus',
+    packages = ['serverlessplus'],
+    version = '0.0.0',
+    license='Apache-2.0',
+    description = 'serverless your django/flask apps',
+    author = 'chenhengqi',
+    author_email = 'ritchiechen@tencent.com',
+    url = 'https://github.com/serverlessplus/py',
+    keywords = ['serverless', 'django', 'flask'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
